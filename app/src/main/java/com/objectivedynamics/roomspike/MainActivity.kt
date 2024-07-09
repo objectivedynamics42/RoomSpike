fee165137c64694e14c767495e3d6806480ecc0f
Michael Baker <objectivedynamics@gmail.com>
2024-07-09 15:26:44 +0100

    Demo writing Room-persisted data to logcat

--- app/src/main/java/com/objectivedynamics/roomspike/MainActivity.kt
+++ app/src/main/java/com/objectivedynamics/roomspike/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.objectivedynamics.roomspike
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,9 +12,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.objectivedynamics.roomspike.ui.theme.RoomSpikeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var repository: WordRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,6 +31,18 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+            }
+        }
+
+        val wordRoomDatabase = WordRoomDatabase.getDatabase(applicationContext, lifecycleScope)
+        val wordDao = wordRoomDatabase.wordDao()
+        repository = WordRepository(wordDao)
+
+        lifecycleScope.launch {
+            val words = repository.allWords.collect {
+                wordsList ->
+                val message = "Words from database: $wordsList"
+                Log.d("RoomDatabase", message)
             }
         }
     }
